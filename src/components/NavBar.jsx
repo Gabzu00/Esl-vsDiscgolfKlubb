e8db962a5172de42d13ed28928755fb86c8e72f6
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-04-26 17:28:55 +0200

    Fixed general styling for all pages. Fixed some additional styling for the navbar and banor page.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './navbar.css'
 
 function NavBar() {
+  const location = useLocation()
+
   return (<>
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand as={Link} to="/">Eslövs Diskgolfklubb</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">Start</Nav.Link>
-          <Nav.Link as={Link} to="/banor">Banor</Nav.Link>
-          <Nav.Link as={Link} to="/medlemskap">Medlemskap</Nav.Link>
+        <Nav className="me-auto" activeKey={location.pathname}>
+          <Nav.Link as={Link} to="/" eventKey="/">Start</Nav.Link>
+          <Nav.Link as={Link} to="/banor" eventKey="/banor">Banor</Nav.Link>
+          <Nav.Link as={Link} to="/medlemskap" eventKey="/medlemskap">Medlemskap</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
