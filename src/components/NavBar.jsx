d75b8576ad511ce4b164e0153e611e835f3d3b17
Gabriel Petersson <gabriel@littleland.se>
2023-05-02 13:33:23 +0200

    changed nav-bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,28 +24,20 @@ function NavBar() {
           <Nav.Link as={Link} to="/" eventKey="/">Start</Nav.Link>
           <Nav.Link as={Link} to="/banor" eventKey="/banor">Banor</Nav.Link>
           <Nav.Link as={Link} to="/medlemskap" eventKey="/medlemskap">Medlemskap</Nav.Link>
-          <Nav.Link as={Link} to="/login" eventKey="/login">Login/Register</Nav.Link>
+          <div className='loginner'>
+            <Nav.Link as={Link} to="/login" eventKey="/login">Logga in </Nav.Link>
+
+            <Navbar.Brand href="/login">
+              <img
+                src="Images.jpg\LoginIcon.png"
+                width="25"
+                height="25"
+                className="logga"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+          </div>
           <Nav.Link as={Link} to="/kontakt" eventKey="/kontakt">Om oss</Nav.Link>
-          <Navbar.Brand href="/login">
-            <img
-              src="Images.jpg\LoginIcon.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
