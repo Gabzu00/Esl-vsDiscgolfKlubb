028a5223d345fe1ed307517955db8f5bec5ff83a
Gabriel Petersson <gabriel@littleland.se>
2023-05-05 21:58:45 +0200

    working backend...

--- db.js
+++ db.js
@@ -1,4 +1,132 @@
-import express from 'express';
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
+dotenv.config()
+
+let dbConnection;
+
+export function getData() {
+  return dbConnection
+    .collection("Albums")
+    .find()
+    .toArray()
+    .then(albums => {
+      return albums;
+    })
+    .catch(error => {
+      throw new Error("Could not fetch the documents: " + error);
+    });
+}
+
+export function getDataTitle(title) {
+  console.log(title)
+  return dbConnection
+    .collection("Albums")
+    .find({ title: { $eq: title } })
+    .toArray()
+    .then(albums => {
+      console.log(albums + " array from db")
+      return albums;
+    })
+    .catch(error => {
+      throw new Error("Could not fetch the documents: " + error);
+    });
+}
+
+export function addData(_id, title, artistName, year) {
+  return dbConnection
+    .collection("Albums")
+    .insertOne({ _id, title, artistName, year })
+    .then(result => {
+      console.log(result.insertedCount + " document(s) inserted");
+      return result;
+    })
+    .catch(error => {
+      return ("The id you entered already exist in the database")
+      /*  throw new Error("Could not insert the document: " + error); */
+    });
+}
+
+export function updateData(_id, title, artistName, year) {
+  console.log(_id)
+  return dbConnection
+    .collection("Albums")
+    .updateOne(
+      { _id },
+      { $set: { title, artistName, year } }
+    )
+    .then(result => {
+      if (result.modifiedCount === 1) {
+        console.log("Document updated successfully");
+        return result;
+      } else {
+        return "Error ! This id was not found in the database"
+      }
+
+    })
+    .catch(error => {
+      console.log("Could not update the document:", error);
+      throw error;
+    });
+}
+
+export function deleteData(_id) {
+  console.log(_id)
+  return dbConnection
+    .collection("Albums")
+    .deleteOne({ _id })
+    .then(result => {
+      if (result.deletedCount === 1) {
+        console.log("Document deleted successfully");
+        return "Document deleted successfully";
+      } else {
+        return "Error ! This id was not found in the database"
+      }
+    })
+    .catch(error => {
+      console.log("Could not delete the document:", error);
+      throw error;
+    });
+}
+
+export async function connectToDB() {
+  try {
+    const client = await MongoClient.connect(process.env.dbUrl);
+    dbConnection = client.db();
+    console.log("Connected!!");
+  } catch (error) {
+    console.log(error + " Hello");
+    throw new Error("Could not connect to database: " + error);
+  }
+}
+
+export function getDB() {
+  return dbConnection;
+}
+
+
+/* module.exports = {
+  deleteData,
+  updateData,
+  addData,
+  getData,
+  getDataTitle,
+  connectToDB: async () => {
+    try {
+      const client = await MongoClient.connect(process.env.URL);
+      dbConnection = client.db();
+      console.log("Connected!!");
+    } catch (error) {
+      console.log(error);
+      throw new Error("Could not connect to database: " + error);
+    }
+  },
+  getDB: () => dbConnection,
+}; */
+
+
+
+
+/* import express from 'express';
 import mongoose from 'mongoose';
 
 import dotenv from 'dotenv'
@@ -13,7 +141,9 @@ const dbApp = express();
 dbApp.use(express.json());
 dbApp.use(express.urlencoded({ extended: false }));
 dbApp.use(router);
-dbApp.use(cors());
+dbApp.use(cors({
+  origin: "*"
+}));
 
 dbApp.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -152,4 +282,4 @@ dbApp.delete('/users/:userName', async (req, res) => {
     res.status(404).json('User not found');
   }
 
-})
+}) */
---
+++ server.js
@@ -0,0 +1,113 @@
+import express from "express";
+import { json } from "express";
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+import { join } from 'path';
+import { addData, connectToDB, getDB, deleteData, updateData, getDataTitle, getData } from "./db.js";
+import cors from "cors";
+
+app.use(json())
+
+app.use(cors({
+  origin: "*"
+}))
+
+let db
+
+connectToDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("app listening...")
+    })
+    db = getDB()
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+/* app.use(express.static(join(__dirname, 'public'))); */
+
+app.get('/', (req, res) => {
+  /* const filePath = path.join(__dirname, 'index.html'); */
+  res.sendFile(join(__dirname, '/index.html'))
+})
+
+app.get('/users', (req, res) => {
+  getData()
+    .then(albums => {
+      /* getData(albums); */
+      res.json(albums)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+})
+
+app.get('/api/albums/:title', (req, res) => {
+  const title = req.params.title;
+  getDataTitle(title)
+    .then(albums => {
+      console.log(albums + " from server")
+      if (albums === undefined || albums.length == 0) {
+        res.send(JSON.stringify("title not found")).status(404)
+      } else {
+        res.json(albums)
+      }
+
+    })
+    .catch(error => {
+      res.json(error)
+    });
+
+})
+
+
+app.post('/api/albums', (req, res) => {
+  let id = req.body.id
+  let title = req.body.title
+  let artist = req.body.artist
+  let year = req.body.year
+
+  addData(id, title, artist, year)
+    .then(albums => {
+      console.log(albums)
+      res.json(albums).status(201)
+    })
+    .catch(error => {
+      res.json(error).status(409)
+    });
+})
+
+
+app.put('/api/albums/:id', (req, res) => {
+  let id = req.params.id
+  let title = req.body.title
+  let artist = req.body.artist
+  let year = req.body.year
+
+  updateData(id, title, artist, year)
+    .then(albums => {
+      console.log(albums)
+      res.json(albums).status(201)
+    })
+    .catch(error => {
+      res.json(error).status(404)
+    });
+
+})
+
+app.delete('/api/albums/:id', (req, res) => {
+  let id = req.params.id
+
+  deleteData(id)
+    .then(albums => {
+      console.log(albums)
+      res.json(albums).status(201)
+    })
+    .catch(error => {
+      res.json(error).status(404)
+    });
+
+
+})
