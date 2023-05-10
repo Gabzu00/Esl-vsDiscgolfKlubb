2137b8f26b875f1dcdf0776119ac6ac9d629fcb1
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-05-10 16:04:42 +0200

    test

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './admin.css'
-import { Container, Row, Col, Table } from 'react-bootstrap'
+import { Container, Row, Col, Table, Button } from 'react-bootstrap'
+
+// Make this variable 'http://localhost:3000' when in dev environment
+// Make empty ('') before pushing/merging
+const DEV_VARIABLE = 'http://localhost:3000'
 
 function Admin() {
 
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('/users', {
+    fetch(DEV_VARIABLE + '/users', {
       method: 'GET',
       crossorigin: true,
     }).then(response => response.json())
@@ -35,12 +39,25 @@ function Admin() {
               <td>{user.firstName}</td>
               <td>{user.lastName}</td>
               <td>{user.email}</td>
+              <td><Button variant='dark' onClick={() => handleRemoveUser(user._id)}>Remove</Button></td>
             </tr>
           ))}
         </tbody>
       </Table>
     </main>
   )
+}
+
+// Gets a user based on id and removes them from the database
+function handleRemoveUser(id) {
+  fetch(DEV_VARIABLE + `/users/${id}`, {
+    method: 'DELETE',
+    headers: { 'content-type': 'application/json' },
+    crossorigin: true,
+  }).then(response => {
+    console.log('RESPONSE', response)
+    return response.json()
+  })
 }
 
 export default Admin;
