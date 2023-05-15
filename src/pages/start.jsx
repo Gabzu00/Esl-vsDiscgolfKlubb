99dc19b4de8782dfcc5a7d9ab98532e22f6ba18c
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-05-15 21:07:43 +0200

    Fixed minor problem

--- src/pages/start.jsx
+++ src/pages/start.jsx
@@ -6,7 +6,7 @@ function Start() {
         <h1>Välkommen</h1>
         <p>Sedan discgolfbanan anlades i Trollsjöområdet i Eslöv har ett antal spelare samlats varje vecka för att spela.
           Detta resulterade i att vi bildade en förening i maj 2009 under namnet Frisbeesport Eslöv. 2022 bytte klubben namn till Eslövs Discgolf Klubb.</p>
-        <p>Klubben samlas och spelar på vår bana i Eslöv på tisdagar 18:00 under sommaren och söndagar 10:00 året runt. För information om vår bara se Banor. För att bli medlem se Medlemskap</p>
+        <p>Klubben samlas och spelar på vår bana i Eslöv på tisdagar 18:00 under sommaren och söndagar 10:00 året runt. För information om vår bana se Banor. För att bli medlem se Medlemskap</p>
       </main>
     </>
   )
